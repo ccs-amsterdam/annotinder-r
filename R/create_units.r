b186e7b37a1b4a595f439f0043a7aad9b1db5bb6
Kasper Welbers <kasperwelbers@gmail.com>
2022-06-17 17:39:11 +0200

    refactoring

--- R/create_units.r
+++ R/create_units.r
@@ -13,14 +13,17 @@
 #' @export
 #'
 #' @examples
-create_units <- function(data, id, variables=NULL) {
+create_units <- function(data, id, type=NULL, variables=NULL) {
   if (!id %in% colnames(data)) stop(sprintf('"%s" is not a column name in d', id))
+  if ('.TYPE' %in% colnames(data)) stop('data cannot have a column called ".TYPE" (what a coincidence, right?)')
   if (anyDuplicated(data[[id]])) stop(sprintf('The id column (%s) needs to have unique values', id))
   for (variable in variables) {
     if (!variable %in% colnames(d)) stop(sprintf('"%s" is not a column name in d', variable))
   }
+  if (!is.null(type))
 
-  l = list(df = dplyr::as_tibble(data), id=id, variables=variables)
+  l = list(df = dplyr::as_tibble(data), id=id, fields=c(), variables=variables)
+  l$df$.TYPE = 'code'
 
   structure(l, class = c('createUnitsBundle', 'list'))
 }
@@ -28,22 +31,41 @@ create_units <- function(data, id, variables=NULL) {
 function() {
   data = data.frame(id = c(1,2,3,4,5),
                     letter = letters[1:5],
+                    date=c('2020-01-01','2020-01-02','2020-01-03','2020-01-04','2020-01-05'),
+                    source=c('imagination'),
+                    title=c('Cat','Cat','Dog','Dog','Car'),
                     text= c('I like cats.',
                             "Cats are awesome.",
                             "Some people like dogs.",
                             "Dogs are pretty awesome too.",
                             "Other people like cars"),
+                    image=c('https://cdn.pixabay.com/photo/2017/07/25/01/22/cat-2536662_960_720.jpg',
+                            'https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_960_720.jpg',
+                            'https://cdn.pixabay.com/photo/2018/01/09/11/04/dog-3071334_960_720.jpg',
+                            'https://cdn.pixabay.com/photo/2017/09/25/13/14/dog-2785077_960_720.jpg',
+                            'https://cdn.pixabay.com/photo/2016/11/29/09/32/auto-1868726_960_720.jpg'),
+                    caption=c('Cat!','Caaaaaat','Doggie!!','Dog','Crrr'),
+                    markdown=c('**useless markdown text**'),
                     animal=c('Cat',NA,'Dog',NA, 'Neither :('))
 
-create_units(data, 'id') |>
+
+units = create_units(data, 'id') |>
+  set_meta('source') |>
+  set_meta('date') |>
+  set_text('title', size=1.3, bold=T) |>
   set_text('text') |>
-  set_meta('animal')
+  set_image('image', caption='caption') |>
+  set_markdown('markdown', align='center')
+
+prepare_units(units, NULL)
 }
+
+
 
 #' Set text content
 #'
 #' @param data        A createUnitsBundle object, as created with \code{\link{create_units}}
-#' @param column The name of a "character" column in the data that contains coding unit text.
+#' @param column      The name of a "character" column in the data that contains coding unit text.
 #'                    Note that this can also be empty if the current text field is only context (before or after the coding unit).
 #'                    For example, the data.frame could be a keyword in context listing with the columns "pre", "keyword" and "post" (see for instance quanteda's kwic function).
 #'                    These could then be set to the "before", "column" and "after" arguments, respectively.
@@ -62,8 +84,12 @@ create_units(data, 'id') |>
 #'
 #' @examples
 set_text <- function(data, column=NULL, before=NULL, after=NULL, label=NULL, ..., offset=0) {
+  if (!is.null(column)) column = name_as_character(substitute(column))
+  if (!is.null(before)) before = name_as_character(substitute(before))
+  if (!is.null(after)) after = name_as_character(substitute(after))
+
   for (col in c(column, before, after, label)) {
-    if (!col %in% colnames(data)) stop(sprintf('"%s" is not a column name in data', col))
+    if (!col %in% colnames(data$df)) stop(sprintf('"%s" is not a column name in data', col))
   }
   if (is.null(column)) {
     if (is.null(before) && is.null(after)) stop('at least one of "column", "before" or "after" needs to be specified')
@@ -83,6 +109,8 @@ set_text <- function(data, column=NULL, before=NULL, after=NULL, label=NULL, ...
 
   if (is.null(data$text)) data$text = list()
   data$text[[length(data$text)+1]] = l
+  if (field %in% data$fields) stop(sprintf('field name (%s) already exists', field))
+  data$fields = c(data$fields, field)
   data
 }
 
@@ -91,23 +119,27 @@ set_text <- function(data, column=NULL, before=NULL, after=NULL, label=NULL, ...
 #' @param data        A createUnitsBundle object, as created with \code{\link{create_units}}
 #' @param column      The column with the meta data
 #' @param label      A character value to label the meta field.
+#' @param bold       Meta data by default uses 'bold' style setting.
 #' @param ...        Style settings, passed to \code{\link{style}}
 #'
 #' @return A createUnitsBundle object
 #' @export
 #'
 #' @examples
-set_meta <- function(data, column, label=NULL, ...) {
+set_meta <- function(data, column, label=NULL, bold=TRUE, ...) {
+  column = name_as_character(substitute(column))
   for (col in c(column, label)) {
-    if (!col %in% colnames(data)) stop(sprintf('"%s" is not a column name in data', col))
+    if (!col %in% colnames(data$df)) stop(sprintf('"%s" is not a column name in data', col))
   }
 
   l = list(field = column,
-           style = style(...),
+           style = style(bold=bold, ...),
            label=label)
 
   if (is.null(data$meta)) data$meta = list()
   data$meta[[length(data$meta)+1]] = l
+  if (column %in% data$fields) stop(sprintf('field name (%s) already exists', column))
+  data$fields = c(data$fields, column)
   data
 }
 
@@ -115,7 +147,7 @@ set_meta <- function(data, column, label=NULL, ...) {
 #'
 #' @param data        A createUnitsBundle object, as created with \code{\link{create_units}}
 #' @param column      The name of a column in data that contains paths/urls to image files
-#' @param caption    A character value, to be shown as image caption
+#' @param caption     The name of a column in data that contains the image caption
 #' @param ...        Style settings, passed to \code{\link{style}}
 #'
 #' @return A createUnitsBundle object
@@ -123,17 +155,19 @@ set_meta <- function(data, column, label=NULL, ...) {
 #'
 #' @examples
 set_image <- function(data, column, caption=NULL, ...) {
+  column = name_as_character(substitute(column))
   for (col in c(column, caption)) {
-    if (!col %in% colnames(data)) stop(sprintf('"%s" is not a column name in data', col))
+    if (!col %in% colnames(data$df)) stop(sprintf('"%s" is not a column name in data', col))
   }
 
   l = list(field = column,
-           style = style(...),
-           label=label)
+           style = style(...))
   if (!is.null(caption)) l$caption = caption
 
   if (is.null(data$image)) data$image = list()
   data$image[[length(data$image)+1]] = l
+  if (column %in% data$fields) stop(sprintf('field name (%s) already exists', column))
+  data$fields = c(data$fields, column)
   data
 }
 
@@ -148,8 +182,9 @@ set_image <- function(data, column, caption=NULL, ...) {
 #'
 #' @examples
 set_markdown <- function(data, column, ...) {
+  column = name_as_character(substitute(column))
   for (col in c(column)) {
-    if (!col %in% colnames(data)) stop(sprintf('"%s" is not a column name in data', col))
+    if (!col %in% colnames(data$df)) stop(sprintf('"%s" is not a column name in data', col))
   }
 
   l = list(field = column,
@@ -157,8 +192,49 @@ set_markdown <- function(data, column, ...) {
 
   if (is.null(data$markdown)) data$markdown = list()
   data$markdown[[length(data$markdown)+1]] = l
+  if (column %in% data$fields) stop(sprintf('field name (%s) already exists', column))
+  data$fields = c(data$fields, column)
   data
 }
+
+test <- function(x) {
+  print(as.character(substitute(x)))
+  print(class(substitute(x)))
+}
+
+test(x = banaan)
+
+#' Set training units
+#'
+#' @param data        A createUnitsBundle object, as created with \code{\link{create_units}}
+#' @param select      A logical expression to select which units are training units. Perhaps the best approach is to
+#'                    already have a column in the data that indicates the type of units (code, train or test). The select
+#'                    expression would then be something like: type == 'train'.
+#' @param ...         \code{\link{condition}} functions, to indicate
+#'
+#' @return
+#' @export
+#'
+#' @examples
+set_train <- function(data, select) {
+  s = substitute(select)
+  data$.TYPE = ifelse(eval(s, data), 'train', data$.TYPE)
+  eval(s, data)
+}
+
+test <- function(x ) {
+  print(as.character(substitute(x)))
+  print(class(substitute(x)))
+
+}
+
+name_as_character <- function(substituted) {
+  if (class(substituted)[1] %in% c('name', 'character'))
+    as.character(substituted)
+  else
+    eval(substituted)
+}
+
 
 #' S3 print method for createUnitsBundle objects
 #'
@@ -169,10 +245,10 @@ set_markdown <- function(data, column, ...) {
 #' @examples
 #' @export
 print.createUnitsBundle <- function(x, ...){
-  print(x$df)
-  for (text in x$text) {
-    cat(text, '\n')
-  }
+  units = table(x$df$.TYPE)
+  units = units[match(unique(x$df$.TYPE), names(units))] # sort by occurence
+  cat(sprintf('- units:'), paste(paste0(names(units), ' (', units, ')'), collapse=', '))
+  cat(sprintf('\n- fields: %s', paste(x$fields, collapse=', ')))
 }
 
 #' Set styling
---
+++ R/prepare_units.r
@@ -0,0 +1,111 @@
+prepare_units <- function(createUnitsBundle, annotations=NULL) {
+  d = createUnitsBundle$df
+
+  if (is.null(annotations) || nrow(annotations) == 0) {
+    ann_list = NULL
+  } else {
+    ann_list = split(annotations, annotations$id)
+    ann_list = ann_list[match(d[[createUnitsBundle$id]], names(ann_list))]
+  }
+
+  units = vector('list', nrow(d))
+  for (i in 1:nrow(d)) {
+    rowdict = as.list(d[i,])
+    id = rowdict[[createUnitsBundle$id]]
+    text_fields = create_text_fields(rowdict, createUnitsBundle$text)
+    meta_fields = create_meta_fields(rowdict, createUnitsBundle$meta)
+    image_fields = create_image_fields(rowdict, createUnitsBundle$image)
+    markdown_field = create_markdown_field(rowdict, createUnitsBundle$markdown)
+    variables = create_variables(rowdict, createUnitsBundle$variables)
+
+    gold = create_gold(rowdict, createUnitsBundle$gold)
+    importedAnnotations = create_imported_annotations(ann_list[[i]])
+
+    ## to do: add "gold". Then when creating coding job check if gold answers match with questions
+    units[[i]] = list(id = jsonlite::unbox(id),
+                      type = d$.TYPE,
+                      unit = list(text_fields=text_fields,
+                                  meta_fields=meta_fields,
+                                  image_fields=image_fields,
+                                  markdown_field=markdown_field,
+                                  variables=variables))
+
+    if (!is.null(importedAnnotations)) units[[i]]$unit$importedAnnotations = importedAnnotations
+    if (!is.null(gold)) units[[i]]$gold = gold
+
+  }
+
+  structure(units, class=c('codingjobUnits', 'list'))
+}
+
+create_text_fields <- function(rowdict, text_cols) {
+  lapply(seq_along(text_cols), function(i) {
+    tf = text_cols[[i]]
+    text = if (is.null(tf$coding_unit)) '' else rowdict[[tf$coding_unit]]
+    text_field = list(name=tf$field, value=text, style=tf$style)
+    if (!is.null(tf$label)) text_field$label = tf$label
+    if (!is.null(tf$context_before)) text_field$context_before = rowdict[[tf$context_before]]
+    if (!is.null(tf$context_after)) text_field$context_after = rowdict[[tf$context_after]]
+    text_field
+  })
+}
+
+
+create_meta_fields <- function(rowdict, meta_cols) {
+  lapply(seq_along(meta_cols), function(i) {
+    mf = meta_cols[[i]]
+    meta_field = list(name = mf$field, value=rowdict[[mf$field]], style=mf$style)
+    if (!is.null(mf$label)) meta_field$label = mf$label
+    meta_field
+  })
+}
+
+create_image_fields <- function(rowdict, image_cols) {
+  lapply(seq_along(image_cols), function(i) {
+    rf = image_cols[[i]]
+    image_field = list(name = rf$field, value=rowdict[[rf$field]], style=rf$style)
+    if (!is.null(rf$caption)) image_field$caption = rf$caption
+    image_field
+  })
+}
+
+create_markdown_field <- function(rowdict, markdown_cols) {
+  lapply(seq_along(markdown_cols), function(i) {
+    mf = markdown_cols[[i]]
+    markdown_field = list(name = mf$field, value=rowdict[[mf$field]], style=mf$style)
+    markdown_field
+  })
+}
+
+create_variables <- function(rowdict, variable_cols) {
+  l = list()
+  for (vc in variable_cols) {
+    l[[vc]] = rowdict[[vc]]
+  }
+  l
+}
+
+create_imported_annotations <- function(ann) {
+  if (is.null(ann) || nrow(ann) == 0) return(NULL)
+  ann = ann[,c('field','variable','value','offset','length')]
+  ann = apply(ann, 1, function(x) {
+    l = as.list(x)
+    l$offset = as.numeric(l$offset)
+    l$length = as.numeric(l$length)
+    l = lapply(l, jsonlite::unbox)
+  })
+  names(ann) = NULL
+  ann
+}
+
+#' S3 print method for codingjobUnits objects
+#'
+#' @param x an codingjobUnits object, created with \link{create_units}
+#' @param ... not used
+#'
+#' @method print codingjobUnits
+#' @examples
+#' @export
+print.codingjobUnits <- function(x, ...){
+  cat('List of', length(x), 'units\n')
+}
